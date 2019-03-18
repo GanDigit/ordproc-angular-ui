d8e3476d75aad613711bddcdb29469fc878dbcbb
jeyagandhi <gandigit@gmail.com>
2019-03-19 00:47:06 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,83 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductComponent } from './product-app/product/product.component';
+import { ProductAddComponent } from './product-app/product-add/product-add.component';
+import { ProductDetailComponent } from './product-app/product-detail/product-detail.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ProductEditComponent } from './product-app/product-edit/product-edit.component';
+import { OrderComponent } from './order-app/order/order.component';
+import { OrderAddComponent } from './order-app/order-add/order-add.component';
+import { OrderDetailComponent } from './order-app/order-detail/order-detail.component';
+import { OrderEditComponent } from './order-app/order-edit/order-edit.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent,
+    data: { title: 'Product List' }
+  },
+  {
+    path: 'product-details/:id',
+    component: ProductDetailComponent,
+    data: { title: 'Product Details' }
+  },
+  {
+    path: 'product-add',
+    component: ProductAddComponent,
+    data: { title: 'Product Add' }
+  },
+  {
+    path: 'product-edit/:id',
+    component: ProductEditComponent,
+    data: { title: 'Product Edit' }
+  },
+  { path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  },
+  {
+    path: 'orders',
+    component: OrderComponent,
+    data: { title: 'Order List' }
+  },
+  {
+    path: 'order-details/:id',
+    component: OrderDetailComponent,
+    data: { title: 'Order Details' }
+  },
+  {
+    path: 'order-add',
+    component: OrderAddComponent,
+    data: { title: 'Order Add' }
+  },
+  {
+    path: 'order-edit/:id',
+    component: OrderEditComponent,
+    data: { title: 'Order Edit' }
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductAddComponent,
+    ProductDetailComponent,
+    ProductEditComponent,
+    OrderComponent,
+    OrderAddComponent,
+    OrderDetailComponent,
+    OrderEditComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-app/rest.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+
+const endpoint = 'http://9.204.168.81:30371/';
+const endpoint2 = 'http://localhost:8082/';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(private http: HttpClient) { }
+
+  private extractData(res: Response) {
+    let body = res;
+    console.log('extractData--> ${res}')
+    return body || { };
+  }
+
+  getProducts(): Observable<any> {
+    console.log('getProducts--> sdf')
+    return this.http.get(endpoint + 'products').pipe(
+      map(this.extractData));
+  }
+  
+  getProduct(id): Observable<any> {
+    return this.http.get(endpoint + 'products/' + id).pipe(
+      map(this.extractData));
+  }
+  
+  addProduct (product): Observable<any> {
+    console.log("Add product ---> " + JSON.stringify(product));
+    return this.http.put<any>(endpoint + 'products', JSON.stringify(product), httpOptions).pipe(
+      tap((product) => console.log(`added product w/ id=${product.id}`)),
+      catchError(this.handleError<any>('addProduct'))
+    );
+  }
+  
+  updateProduct (id, product): Observable<any> {
+    return this.http.put(endpoint + 'products', JSON.stringify(product), httpOptions).pipe(
+      tap(_ => console.log(`updated product id=${id}`)),
+      catchError(this.handleError<any>('updateProduct'))
+    );
+  }
+  
+  deleteProduct (id): Observable<any> {
+    return this.http.delete<any>(endpoint + 'products/' + id, httpOptions).pipe(
+      tap(_ => console.log(`deleted product id=${id}`)),
+      catchError(this.handleError<any>('deleteProduct'))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
+
+
